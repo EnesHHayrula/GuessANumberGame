1a55fef528a12b9dcc530245ebf062172da3a85f
EnesHHayrula <116758797+EnesHHayrula@users.noreply.github.com>
2023-09-15 00:27:05 +0300

    Add files via upload

---
+++ HighOrLow.js
@@ -0,0 +1,35 @@
+function highOrLow(number) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let computerGuess = Math.floor(Math.random() * 100);
+    let guess = 0;
+
+    let recursiveAsyncReadLine = function () {
+        readline.question(`Guess the number (0-100):`, number => {
+            guess = Number(number)
+
+            if (guess <= 100 && guess >= 0) {
+                if (guess == computerGuess) {
+                    console.log(`You guess it!`);
+                    return readline.close();
+                } else if (guess < computerGuess) {
+                    console.log(`Too Low!`);
+                    recursiveAsyncReadLine()
+                } else if (guess > computerGuess) {
+                    console.log(`Too High!`);
+                    recursiveAsyncReadLine()
+                }
+            } else {
+                console.log(`Invalid input! Try again...`);
+                recursiveAsyncReadLine()
+            }
+        });
+    }
+
+    recursiveAsyncReadLine();
+}
+
+highOrLow(50)
